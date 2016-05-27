14def2ff42bf6bbcc4b4881d72afe7e2850eb296
lw334 <lingwei9334@gmail.com>
2016-05-27 02:30:58 -0500

    retrieve mentions; prep for cron

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,3 +4,4 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 end
+
--- app/models/mention.rb
+++ app/models/mention.rb
@@ -6,15 +6,20 @@ class Mention < ActiveRecord::Base
     @client = twitter.client
     mentions = @client.mentions_timeline
     mentions.each do |tweet|
-      puts tweet.in_reply_to_screen_name
-      ## m = Mention.new
-      #  m.content =
-      #  m.save
+      m = Mention.new
+      m.in_reply_to_screen_name = tweet.in_reply_to_screen_name
+      m.favorite_count = tweet.favorite_count
+      m.in_reply_to_user_id = tweet.in_reply_to_screen_name
+      m.lang = tweet.lang
+      m.retweet_count = tweet.retweet_count
+      m.text = tweet.text
+      hash_tags = tweet.text.scan(/#\S+/)
+      m.hash_tag = hash_tags.uniq!
+      m.save
     end
-
   end
-end
 
+end
 
 # @mention = Mention.new
 # @mention.method
