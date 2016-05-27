94ad7079ea5410e4d26a04fafeeebe46f7239d3c
lw334 <lingwei9334@gmail.com>
2016-05-27 01:14:31 -0500

    remove credential

--- app/models/twitter_api.rb
+++ app/models/twitter_api.rb
@@ -3,10 +3,10 @@ class Twitter_API
 
   def initialize
     @client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = "DXbq8HDvnQO0PFMaIyrBVnmO7"
-      config.consumer_secret     = "c8747X0QmIlXImqyVlf6S1YfoKqLBjQ0AiNetLS29M0HyHbd8R"
-      config.access_token        = "729902640059056128-AOr1a3IakmF2Pc6OiQzDLzzuJq7IvD4"
-      config.access_token_secret = "UU1PPcWWNAq7vBkCcDj1BK8f075rtCSWVoihEftJgnNsO"
+      config.consumer_key        = ""
+      config.consumer_secret     = ""
+      config.access_token        = ""
+      config.access_token_secret = ""
     end
   end
 end
