79a980f762372021618d44734cd16d64e5671ede
lw334 <lingwei9334@gmail.com>
2016-05-29 01:21:15 -0500

    mer

--- app/models/twitter_api.rb
+++ app/models/twitter_api.rb
@@ -3,12 +3,11 @@ class Twitter_API
 
   def initialize
     @client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ""
-      config.consumer_secret     = ""
-      config.access_token        = ""
-      config.access_token_secret = ""
+      config.consumer_key = ENV['CONSUMER_KEY']
+      config.consumer_secret = ENV['CONSUMER_SECRET']
+      config.access_token = ENV['YOUR_ACCESS_TOKEN']
+      config.access_token_secret = ENV['YOUR_ACCESS_SECRET']
     end
   end
 end
-
 
