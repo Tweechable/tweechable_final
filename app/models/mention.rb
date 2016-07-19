f1c36ba158f92846f2702f928287d39d88382e37
J Hyde <jkhyde86@gmail.com>
2016-07-19 14:19:51 -0500

    Updated the reply_mentions function in Mention so it can reply to multiple mentions in one go

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -41,27 +41,28 @@ class Mention < ActiveRecord::Base
   end
 
   def self.reply_mentions
-    mention = Mention.where(replied:false).where.not(lesson_id:nil).first
-    @lesson = Lesson.find_by(id: mention.lesson_id)
+    mentions = Mention.where(replied: false).where.not(lesson_id:nil)
+    mentions.each do |mention|
+      @lesson = Lesson.find_by(id: mention.lesson_id)
 
-    if @lesson
-      twitters = Twitter_API.new
-      @client = twitters.client
+      if @lesson
+        twitters = Twitter_API.new
+        @client = twitters.client
 
-      @lesson.tweets.each do |message|
-        # add the blank too make sure we can tag! otherwise it'd be one string
-        to_send = message.text + ' ' + mention.handler
-        puts "Tweeting: #{to_send}"
-        @t = @client.update(to_send)
-        if !@t
-          #If something goes wrong during this process, it should probably do something about it
-          puts "Something went wrong with posting this tweet: #{to_send}"
+        @lesson.tweets.each do |message|
+          # add the blank too make sure we can tag! otherwise it'd be one string
+          to_send = message.text + ' ' + mention.handler
+          puts "Tweeting: #{to_send}"
+          @t = @client.update(to_send)
+          if !@t
+            #If something goes wrong during this process, it should probably do something about it
+            puts "Something went wrong with posting this tweet: #{to_send}"
+          end
         end
+        #Right now this is assuming the reply went successfully
+        mention.update(replied: true);
       end
-      #Right now this is assuming the reply went successfully
-      mention.update(replied: true);
     end
-    
   end
 
 end
