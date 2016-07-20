df9208a836992b89dd0b4e27c3b0bafb6f786abf
J Hyde <jkhyde86@gmail.com>
2016-07-19 20:15:37 -0500

    Removed length validation on tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,6 +4,11 @@ class Tweet < ActiveRecord::Base
 
   # tweet is 140 char max and tweeter name is 15 char max. so we we let 124 to be the max length so that
   # after we add in the (screen name + the blank) to form the tag the tweet is 140 max and allow to be published
-  validates :text, length: { maximum: 124 }
+  # validates :text, length: { maximum: 124 }
+  # validate :tweet_length_below_124
+
+  def tweet_length_below_124
+
+  end
 
 end
