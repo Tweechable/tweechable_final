dc722eb8b0e21c5ef68556806e03c811060bbcb3
Eileen McFarland <eileenm@gmail.com>
2016-08-30 20:05:48 -0500

    do add devise to users migration

--- Gemfile
+++ Gemfile
@@ -36,7 +36,6 @@ gem 'jquery-ui-rails'
 gem 'pg'
 # Use ActiveModel has_secure_password
 gem 'bcrypt'
-# devise potentially..
 gem 'devise'
 # rails admin
 gem 'rails_admin'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,16 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  #validates :email, presence: true
-  #validates :email, email: true
+
+
   validates :name, presence: true
 
   has_secure_password
 
   has_many :contributions
-  has_many :lessons, :through => :contributions
+  has_many :lessons, through: :contributions
 
 end
