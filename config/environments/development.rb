9300737f8c0eba861c1dce98475a740639dc4a96
McEileen <eileenm@gmail.com>
2016-11-11 11:46:06 -0600

    add mailer prefs to development config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,9 @@
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -39,4 +42,5 @@
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
 end
