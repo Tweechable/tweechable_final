470bdfee1a1f0d26af476e4fb3a6059913b5f950
Viktor Koves <viktorkoves@gmail.com>
2016-11-29 20:55:19 -0600

    Added thread_link to lessons Added new thread_link string attribute to lessons for storing a link to the posted thread of tweets that comprise a lesson

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161130024407) do
+ActiveRecord::Schema.define(version: 20161130025438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20161130024407) do
     t.datetime "updated_at"
     t.boolean  "approved",    default: true
     t.string   "intro"
+    t.string   "thread_link"
   end
 
   create_table "mentions", force: :cascade do |t|
