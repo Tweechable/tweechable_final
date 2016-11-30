7f52155d6ffa1639c3249dedaf4381983d9d85b2
Viktor Koves <viktorkoves@gmail.com>
2016-11-29 20:51:33 -0600

    Updated reply_mentions to use thread_link Updated Mention.reply_mentions to use the new lesson.thread_link attribute instead of the old lesson.get_root_link method

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -7,15 +7,4 @@ class Lesson < ActiveRecord::Base
   has_many :educatees
 
   validates :hash_tag, presence:true
-
-  def get_root_link
-  	link = tweets.first.link
-  	if link
-  		return link
-  	else
-  		puts "Root link requested for Lesson with unpublished tweets."
-  		return ""
-  	end
-  end
-
 end
