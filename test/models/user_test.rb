690e9ee361a83749c895702fdb30b8678fa6c73e
Matthew <matthewwgalloway@gmail.com>
2016-12-14 01:56:26 -0600

    Write test for presence of name validation for user model
    Wrote a minitest setup method and a valid user model assertion as well
    as a test asserting that a user without a name should not be valid.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,15 @@
 class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(name: "ChiHacker", password: "password")
+  end
+
   test 'valid user' do
+    assert @user.valid?
   end
 
   test 'invalid without name' do
+    @user.name = nil
+    refute @user.valid?
+    assert_not_nil @user.errors[:name]
   end
 end
