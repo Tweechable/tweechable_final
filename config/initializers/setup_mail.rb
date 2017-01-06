17b7ab03e3f868d99a3eaf369b398f7074868f86
McEileen <eileenm@gmail.com>
2017-01-06 14:00:23 -0600

    remove sendgrid variables

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,8 +3,8 @@ ActionMailer::Base.smtp_settings = {
 	:address 				=>	'smtp.sendgrid.net',
 	:port					=>	'587',
 	:authentication			=>	:plain,
-	:user_name				=>	'app50316764@heroku.com',
-	:password				=>	'oqbbgpwd8191',
+	:user_name				=>	ENV["SENDGRID_USERNAME"],
+	:password				=>	ENV["SENDGRID_PASSWORD"],
 	:domain					=>	'heroku.com',
 	:enable_starttls_auto	=>	true
 }
