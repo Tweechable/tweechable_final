da520efa81f3bbfd4469e4f3593c28e63aee153e
McEileen <eileenm@gmail.com>
2017-03-03 15:03:37 -0600

    fix sign-up form to track how users found tweechable

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -27,7 +27,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   private 
 
   def sign_up_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :how_found_tweechable)
   end
 
   def account_update_params
