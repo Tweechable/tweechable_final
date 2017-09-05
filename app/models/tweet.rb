82ab14ba2ed6d00c15f75b8faf95fc44f5bcda2b
Eileen <eileenm@gmail.com>
2017-09-05 18:53:57 -0500

    reinstate comment about tweet length

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,6 +4,7 @@ class Tweet < ActiveRecord::Base
 
   before_validation :add_index, on: :create
 
+ # tweet is 140 char max and tweeter name is 15 char max. so we we let 124 be the max length so that after we add in the (screen name + the blank) to form the tag the tweet is 140 max and able to be published
   validates :text, length: { maximum: 124 }
 
   # Deletes the previous version of a tweet, if one exists, and posts a new version to twitter
