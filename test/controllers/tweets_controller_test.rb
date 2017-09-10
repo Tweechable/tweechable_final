11459c74ddb8d153b9b705f589b959dae3a37203
Eileen <eileenm@gmail.com>
2017-09-10 15:41:27 -0500

    fix broken tweets_controller tests and make devise mapping clearer

--- test/controllers/tweets_controller_test.rb
+++ test/controllers/tweets_controller_test.rb
@@ -8,6 +8,7 @@ class TweetsControllerTest < ActionController::TestCase
   def setup
     @lesson = lessons(:test_lesson)
     @tweet = tweet(:test_first_tweet)
+    #Controller tests bypass the router, which is where Devise sees what mappings to use before a request. The below lines tell Devise what mappings to use for a request from an admin or a non-admin user.
     @request.env["devise.mapping"] = Devise.mappings[:admin]
   end
 
@@ -17,47 +18,33 @@ class TweetsControllerTest < ActionController::TestCase
   end
 
   test "users signed in as admins can access the page to edit tweets" do
-    @admin = users(:admin)
-    @admin.confirmed_at = DateTime.now
-    @admin.save
     sign_in users(:admin)
     get :edit, id: @tweet.id
     assert_response :success
   end
 
   test "users who are not signed in as admins are redirected when they attempt to edit a lesson" do
-    @non_admin = users(:non_admin)
-    @non_admin.confirmed_at = DateTime.now
-    @non_admin.save
-    sign_in users(:non_admin)
+    sign_in users(:user)
     get :edit, id: @tweet.id
     assert_response :redirect
   end
 
   test "users signed in as admins can update tweets" do
-    skip("test did not work because tweet was sent to update method as 0 - commented out for now")
     @original_contribution_count = Contribution.all.length
-    @admin = users(:admin)
-    @admin.confirmed_at = DateTime.now
-    @admin.save
     sign_in users(:admin)
-    put :update, id: @tweet.id, tweet: @tweet, text: "new content"
+    put :update, id: @tweet.id, tweet: {text: "new content", source: "article"}
     assert_response :redirect
-    assert_redirected_to tweets_url(id: tweet.lesson_id)
-    assert_operator Contribution.all.length > @original_contribution_count
+    assert_redirected_to tweets_url(id: @tweet.lesson_id)
+    assert_operator(Contribution.all.length, :>, @original_contribution_count)
   end
 
-    test "users not signed in as admins cannot update tweets" do
-    skip("test did not work - commented out for now")
+  test "users not signed in as admins cannot update tweets" do
     @original_contribution_count = Contribution.all.length
-    @non_admin = users(:non_admin)
-    @non_admin.confirmed_at = DateTime.now
-    @non_admin.save
-    sign_in users(:non_admin)
-    put :update, params: {id: @tweet.id, tweet: {text: "new content", cited_src: "an article"}}
+    sign_in users(:user)
+    put :update, id: @tweet.id, tweet: {text: "new content", source: "article"}
     assert_response :redirect
     assert_redirected_to lessons_url
-    assert_operator Contribution.all.length == @original_contribution_count
+    assert_operator(Contribution.all.length, :==, @original_contribution_count)
   end
 
 end
