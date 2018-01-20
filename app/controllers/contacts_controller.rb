9b4833ffe686e23803d4024965f15f0e9a54eace
Eileen <eileenm@gmail.com>
2018-01-20 13:52:08 -0600

    update contacts controller with ContactMailer.contact_us

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,6 +8,7 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     if @contact.save
       flash.now[:error] = nil
+      ContactMailer.contact_us(@contact).deliver_now
       flash.now[:notice] = 'Message valid.'
     else
       flash.now[:error] = 'Cannot send message.'
