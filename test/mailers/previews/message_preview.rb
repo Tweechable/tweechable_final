5882839e1ab49559686b6006506a1c49f06efe91
Eileen <eileenm@gmail.com>
2018-01-21 11:56:59 -0600

    delete empty or unnecessary files

--- test/mailers/previews/message_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/message
-class MessagePreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/message/contact_us
-  def contact_us
-    Message.contact_us
-  end
-
-end
