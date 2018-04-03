f70702d35330754061e8d5f19fe289418102530f
McEileen <eileenm@gmail.com>
2018-04-02 22:12:58 -0500

    update mentions test

--- test/models/mentions_test.rb
+++ test/models/mentions_test.rb
@@ -10,26 +10,26 @@ class MentionsTest < ActiveSupport::TestCase
 
 	test "If a user tweets STOP, check_unsubscribe should add them to the Blocklist" do
 		blocks = BlockList.count
-		tweet = generate_tweet("@tweechable STOP")
+		tweet = generate_tweet_that_mentions_only_tweechable("@tweechable STOP", "tweechable", 1)
 		Mention.check_unsubscribes(tweet)
 		assert_equal(blocks + 1, BlockList.count, "The number of blocks should have increased")
 	end
 
 	test "If a tweet doesn't have a hashtag, no mention should be generated" do
-		tweet = generate_tweet("@target hey @tweechable please help test")
+		tweet = generate_tweet_with_mentions("@target hey @tweechable please help test", "tweechable", 1, "target", 2)
 		lesson = Mention.identify_lesson(tweet)
 		assert_nil(lesson, "A lesson shouldn't have been identified")
 	end
 
 	test "If a tweet has a hashtag but it doesn't match any lessons, don't generate a mention" do
-		tweet = generate_tweet("@target hey @tweechable please help #hashtag #blashtag")
+		tweet = generate_tweet_with_mentions("@target hey @tweechable please help #hashtag #blashtag", "tweechable", 1, "target", 2)
 		lesson = Mention.identify_lesson(tweet)
 		assert_nil(lesson, "A lesson shouldn't have been identified")
 	end
 
-test "If a user is on the blocklist as can't receive, mentions shouldn't be generated for them" do
+	test "If a user is on the blocklist as can't receive, mentions shouldn't be generated for them" do
 		no_receive = block_lists(:NoReceive)
-		tweet = generate_tweet("@NoReceive hi @tweechable please help test #test")
+		tweet = generate_tweet_with_mentions("@NoReceive hi @tweechable please help test #test", "tweechable", 1,  "NoReceive", 101)
 		Mention.generate_mention(tweet, "tweechable")
 		assert_equal(0, Mention.count, "No new mentions should have been created")
 	end
@@ -41,43 +41,39 @@ test "If a user is on the blocklist as can't receive, mentions shouldn't be gene
 		assert_equal(0, Mention.count, "No new mentions should have been created")
 	end
 
-# broken test below
-	# test "Given a well formatted tweet, generate_mention should make a mention" do
-	# 		tweet = generate_tweet_with_author_and_mentions("@test_educatee_not_on_blocklist hey @tweechable please help #test", 1, "tweechable_user", 2, "test_educatee_not_on_blocklist")
-	# 	Mention.generate_mention(tweet)
-	# 	assert_equal(1, Mention.count, "A new mention should have been created")
-	# end
+	test "Given a well formatted tweet, generate_mention should make a mention" do
+		tweet = generate_tweet_with_author_and_mentions("@test_educatee_not_on_blocklist hey @tweechable please help #test", 1, "tweechable_user", 2, "test_educatee_not_on_blocklist", 3, "tweechable")
+		Mention.generate_mention(tweet, "tweechable")
+		assert_equal(1, Mention.count, "A new mention should have been created")
+	end
 
-#broken test below
-	# test "Given a well formatted tweet with tweechable first, generate_mention should make a mention" do
-	# 	tweet = generate_tweet("@tweechable please help @test_educatee_not_on_blocklist #test")
-	# 	Mention.generate_mention(tweet)
-	# 	assert_equal(1, Mention.count, "A new mention should have been created")
-	# 	assert_equal(Mention.first.handler, "test_educatee_not_on_blocklist")
-	# end
+	test "Given a well formatted tweet with tweechable first, generate_mention should make a mention" do
+		tweet = generate_tweet_with_mentions("@tweechable please help @test_educatee_not_on_blocklist #test", "tweechable", 1, "test_educatee_not_on_blocklist", 2)
+		Mention.generate_mention(tweet, "tweechable")
+		assert_equal(1, Mention.count, "A new mention should have been created")
+		assert_equal(Mention.first.handler, "test_educatee_not_on_blocklist")
+	end
 
 	test "If a tweet doesn't have a target, don't generate a mention" do
-		tweet = generate_tweet("I think @tweechable is a good service.")
+		tweet = generate_tweet_that_mentions_only_tweechable("I think @tweechable is a good service.", "tweechable", 1)
 		Mention.generate_mention(tweet, "tweechable")
 		assert_equal(0, Mention.count, "A new tweet shouldn't have been created")
 	end
 
-# broken test below
-	# test "A mention shouldn't be generated for an exchange if we've already done that mention" do
-	# 	tweet = generate_tweet("@test_educatee_already_contacted hey @tweechable please help #test")
-	# 	Mention.generate_mention(tweet)
-	# 	assert_equal(1, Mention.count, "A new mention should have been created")
+	test "A mention shouldn't be generated for an exchange if we've already done that mention" do
+		tweet = generate_tweet_with_mentions("@educatee contacted hey @tweechable please help #test", "tweechable", 1, "educatee", 2)
+		Mention.generate_mention(tweet, "tweechable")
+		assert_equal(1, Mention.count, "A new mention should have been created")
 
-	# 	Mention.generate_mention(tweet)
-	# 	assert_equal(1, Mention.count, "A mention shouldn't have been created")
-	# end
+		Mention.generate_mention(tweet, "tweechable")
+		assert_equal(1, Mention.count, "A mention shouldn't have been created")
+	end
 
-# broken test below
-	# test "If there are two valid handles, mentions should be generated for both of them" do
-	# 	tweet = generate_tweet("@target_one @target_two hey @tweechable please help #test")
-	# 	Mention.generate_mention(tweet)
-	# 	assert_equal(2, Mention.count, "Two new mentions should have been created")
-	# end
+	test "If there are two valid handles, mentions should be generated for both of them" do
+		tweet = generate_tweet_with_multiple_educatees("@target_one @target_two hey @tweechable please help #test", "tweechable", 1, "target_one", 2, "target_two", 3)
+		Mention.generate_mention(tweet, "tweechable")
+		assert_equal(2, Mention.count, "Two new mentions should have been created")
+	end
 
 	test "If a mention doesn't have a lesson id associated with it, reply_mentions won't respond to it" do
 		tweet_count_before_test = Tweet.count
@@ -100,11 +96,12 @@ test "If a user is on the blocklist as can't receive, mentions shouldn't be gene
 		assert_equal(tweet_count_before_test, Tweet.count, "A new tweet shouldn't have been created")
 	end
 
-	test "If a mention is well formatted, it will trigger a tweet that contains the potential educatee's handle" do
-		well_formatted_mention = Mention.create(id: 0, created_at: Time.now, favorite_count: 3, lang: "English", retweet_count: 5, text: "a well-formatted mention", hash_tag: "#test", replied: false, lesson_id: 0, handler: "unaware_person")
-		result_tweet = Mention.reply_mentions[0]
-		assert_equal(result_tweet.handler, "unaware_person")
-	end
+# below test currently broken because the Mention.reply_mentions returns mentions, not tweets, and I don't know how to isolate and test the tweet created by this method
+	# test "If a mention is well formatted, it will trigger a tweet that contains the educatee's handle with an @ symbol before it" do
+	# 	well_formatted_mention = Mention.create(id: 0, created_at: Time.now, favorite_count: 3, lang: "English", retweet_count: 5, text: "a well-formatted mention", hash_tag: "#test", replied: false, lesson_id: 0, handler: "unaware_person")
+	# 	result_tweet = Mention.reply_mentions[0]
+	# 	assert_match("unaware_person", result_tweet.text)
+	# end
 
 	test "If a mention is well formatted, it will trigger a tweet that contains the lesson's intro" do
 		well_formatted_mention = Mention.create(id: 0, created_at: Time.now, favorite_count: 3, lang: "English", retweet_count: 5, text: "a well-formatted mention", hash_tag: "#test", replied: false, lesson_id: 0, handler: "unaware_person")
@@ -120,7 +117,7 @@ test "If a user is on the blocklist as can't receive, mentions shouldn't be gene
 	end
 
 	test "If a tweet contains the name of the tweechable bot, tweechable will not tweet at itself" do
-		tweet = generate_tweet_with_author_and_mentions("hi @tweechable please explain #test to @unaware_person", 1, "tweechable_user", 2, "unaware_person", [39, 53], 3, "tweechable", [3, 13])
+		tweet = generate_tweet_with_author_and_mentions("hi @tweechable please explain #test to @unaware_person", 1, "tweechable_user", 2, "unaware_person", 3, "tweechable")
 		Mention.generate_mention(tweet, "tweechable")
 		num_mentions = Mention.all.size
 		assert_equal(1, num_mentions, "There should be only one mention")
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -19,18 +19,76 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
-  def generate_tweet(text)
+  def generate_tweet_that_mentions_only_tweechable(text, bot_name, bot_id)
+      Twitter::Tweet.new({id: 1,
+                  favorite_count: 0,
+                  lang: "en",
+                  retweet_count: 0,
+                  source: "@tweeter",
+                  text: text,
+                  tweet_index: 0,
+                  user: {
+                    id: 0,
+                    screen_name: "tweeter"
+                    },
+                  entities: {
+                    user_mentions: [{
+                      id: bot_id,
+                      screen_name: bot_name
+                      }]}
+                  })
+  end
+
+  def generate_tweet_with_mentions(text, bot_name, bot_id, educatee_name, educatee_id)
   	Twitter::Tweet.new({id: 1,
-						favorite_count: 0,
-            lang: "en",
-						retweet_count: 0,
-						source: "@tweeter",
-						text: text,
-            tweet_index: 0,
-            user: {
-              id: 0,
-              screen_name: "tweeter"
-              }})
+    						favorite_count: 0,
+                lang: "en",
+    						retweet_count: 0,
+    						source: "@tweeter",
+    						text: text,
+                tweet_index: 0,
+                user: {
+                  id: 0,
+                  screen_name: "tweeter"
+                  },
+                entities: {
+                  user_mentions: [{
+                    id: educatee_id,
+                    screen_name: educatee_name
+                    },
+                    {
+                    id: bot_id,
+                    screen_name: bot_name
+                    }]}
+              })
+  end
+
+    def generate_tweet_with_multiple_educatees(text, bot_name, bot_id, first_educatee_name, first_educatee_id, second_educatee_name, second_educatee_id)
+    Twitter::Tweet.new({id: 1,
+                favorite_count: 0,
+                lang: "en",
+                retweet_count: 0,
+                source: "@tweeter",
+                text: text,
+                tweet_index: 0,
+                user: {
+                  id: 0,
+                  screen_name: "tweeter"
+                  },
+                entities: {
+                  user_mentions: [{
+                    id: first_educatee_id,
+                    screen_name: first_educatee_name
+                    },
+                    {
+                    id: second_educatee_id,
+                    screen_name: second_educatee_name
+                    },
+                    {
+                    id: bot_id,
+                    screen_name: bot_name
+                    }]}
+              })
   end
 
   def generate_tweet_author(text, author_id, author_screen_name)
@@ -46,7 +104,7 @@ class ActiveSupport::TestCase
                   }})
   end
 
-  def generate_tweet_with_author_and_mentions(text, author_id, author_screen_name, educatee_id, educatee_screen_name, educatee_indices, bot_id, bot_name, bot_indices)
+  def generate_tweet_with_author_and_mentions(text, author_id, author_screen_name, educatee_id, educatee_screen_name, bot_id, bot_name)
     tweet = Twitter::Tweet.new({id: 1,
                 favorite_count: 0,
                 lang: "en",
@@ -60,16 +118,12 @@ class ActiveSupport::TestCase
                 entities: {
                   user_mentions: [{
                     id: educatee_id,
-                    id_str: educatee_id.to_s,
                     screen_name: educatee_screen_name,
-                    name: educatee_screen_name,
-                    indices: educatee_indices
+                    name: educatee_screen_name
                     },
                     {id: bot_id,
-                    id_str: bot_id.to_s,
                     screen_name: bot_name,
-                    name: bot_name,
-                    indices: bot_indices
+                    name: bot_name
                     }]
                 }
             })
